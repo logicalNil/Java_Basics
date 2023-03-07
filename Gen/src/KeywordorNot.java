4676930a5c7defb0112cfdf8f312237736f2d7b1
nilay <nilayranjan99@gmail.com>
2023-03-07 14:50:13 +0530

    Trainings

---
+++ Gen/src/KeywordorNot.java
@@ -0,0 +1,18 @@
+public class KeywordorNot {
+    public static void main(String[] args) {
+        int num = 4;
+        switch (num) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("three");
+                break;
+            default:
+                System.out.println("not a keyword");
+        }
+    }
+}
