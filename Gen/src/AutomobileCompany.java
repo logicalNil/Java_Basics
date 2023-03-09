a3fdd64de4eaab3bb77c41ea5de58ace3126df91
nilay <nilayranjan99@gmail.com>
2023-03-09 09:25:57 +0530

    Trainings

---
+++ Gen/src/AutomobileCompany.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class AutomobileCompany {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int v = input.nextInt();
+        int w = input.nextInt();
+        float res = ((4*v)-w)/2;
+        if (w<2*v || w%2!=0 || res%1!=0){
+            System.out.println("No Solution");
+        }else{
+            System.out.println((int)res);
+            System.out.println(v-(int)res);
+        }
+    }
+}
