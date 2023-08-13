137ccf71018a26fc6fe7f891c073a3690ffddc26
nilay <nilayranjan99@gmail.com>
2023-08-13 12:52:48 +0530

    New

---
+++ src/Exam/Negativetest.java
@@ -0,0 +1,11 @@
+package Exam;
+
+public class Negativetest {
+    public boolean negativenumbers(int a, int b) {
+        if (a > 0 && b > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
